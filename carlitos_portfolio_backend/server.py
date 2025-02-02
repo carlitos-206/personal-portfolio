6cf9db2498399f73f98f553b337f0d17538750a9
carlitos-206 <carloscaceres041@gmail.com>
2025-02-01 18:46:31 -0800

    connection to backend succesful

--- carlitos_portfolio_backend/server.py
+++ carlitos_portfolio_backend/server.py
@@ -1,15 +1,17 @@
 from flask import Flask, request, jsonify
 from user_agent import generate_user_agent, generate_navigator
-import json
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
-    return {
-        'status': 200,
-        'message': 'Server is online',
-        'active': True
+    response = {
+        "status": 200,
+        "message": "Successful connection to backend"
     }
+    return jsonify(response)
 
 @app.route('/simple-ua')
 def simpleUA():
--- carlitos_portfolio_site/src/app/_components/GLOBAL/checkServer.js
+++ carlitos_portfolio_site/src/app/_components/GLOBAL/checkServer.js
@@ -2,6 +2,7 @@ export const isServerOnline = async () => {
     try {
         const response = await fetch(`https://carlitos.ngrok.dev/`);
         const data = await response.json();
+        console.log(`data: ${data}`)
         return data;
     } catch (error) {
         console.error(error);
