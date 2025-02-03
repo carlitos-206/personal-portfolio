d6686ebd6aa8d54770b5474174552b2cb0cd027f
carlitos-206 <carloscaceres041@gmail.com>
2025-02-02 16:35:31 -0800

    env properly loaded, firebase connected, contact section complete

--- carlitos_portfolio_site/src/app/_components/GLOBAL/functions/checkServer.js
+++ carlitos_portfolio_site/src/app/_components/GLOBAL/functions/checkServer.js
@@ -1,6 +1,6 @@
 export const isServerOnline = async () => {
     try {
-        const response = await fetch(`https://carlitos.ngrok.dev/`);
+        const response = await fetch(`${process.env.NEXT_PUBLIC_PUBLIC_SERVER}/`);
         const data = await response.json();
         console.log(`data: ${data}`)
         return data;
--- carlitos_portfolio_site/src/app/_components/contact/page.js
+++ carlitos_portfolio_site/src/app/_components/contact/page.js
@@ -3,13 +3,26 @@ import { useEffect, useState } from "react";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import AccountCircle from "@mui/icons-material/AccountCircle";
-import { ContactMail, Subject, Engineering } from "@mui/icons-material";
+import { ContactMail, Subject, Engineering, Try } from "@mui/icons-material";
+import { db } from "../GLOBAL/database/firebase"; 
+import { collection, addDoc } from 'firebase/firestore';
+
 import "./layout.css";
 
 export default function Contact() {
     const [index, setIndex] = useState(0);
     const [indexTextBox, setIndexTextBox] = useState(0);
-    const [isMobile, setIsMobile] = useState(null)
+    const [isMobile, setIsMobile] = useState(null);
+    
+    const [fullName, setFullName] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [messageSubject, setMessageSubject] = useState(null);
+    const [messageBody, setMessageBody] = useState(null)
+    const [fullNameError, setFullNameError] = useState(false);
+    const [emailError, setEmailError] = useState(false);
+    const [subjectError, setSubjectError] = useState(false);
+    const [bodyError, setBodyError] = useState(false);
+
     const textValues = ["project", "idea", "app", "event"];
     const textValuesForBox = [
         "your idea",
@@ -90,6 +103,85 @@ export default function Contact() {
     }
 
 
+
+
+
+
+
+
+    const sendMessage = async (e, fName, mail, subject, body ) =>{
+        e.preventDefault()
+        console.log(`
+            fname: ${fName},
+            mail: ${mail},
+            subject: ${subject}
+            body: ${body}
+            `)
+        const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)])/;
+        let hasError = false;
+
+         // Validate full name
+        if (!fullName || fullName.trim().length < 2) {
+            setFullNameError(true);
+            hasError = true;
+        } else {
+            setFullNameError(false);
+        }
+        
+        // Validate email
+        if (!email || !emailRegex.test(email)) {
+            setEmailError(true);
+            hasError = true;
+        } else {
+            setEmailError(false);
+        }
+    
+        // Validate subject
+        if (!messageSubject || messageSubject.trim().length < 4) {
+            setSubjectError(true);
+            hasError = true;
+        } else {
+            setSubjectError(false);
+        }
+    
+        // Validate message body
+        if (!messageBody || messageBody.trim().length < 21) {
+            setBodyError(true);
+            hasError = true;
+        } else {
+            setBodyError(false);
+        }
+    
+        // If any error exists, do not proceed.
+        if (hasError) {
+            return;
+        } else{
+            setFullNameError(false)
+            setEmailError(false)
+            setSubjectError(false)
+            setBodyError(false)
+            // send off to Firebase
+            try{
+                const docRef = await addDoc( collection(db, 'contact-section'), {
+                    full_name: fullName,
+                    email: email,
+                    subject: messageSubject,
+                    message: messageBody,
+                    date: new Date().toLocaleString("en-US", {  timeZone: "America/Los_Angeles"})
+                })
+                setFullName("")
+                setEmail("")
+                setMessageSubject("")
+                setMessageBody("")
+            }catch (e) {
+                alert(`
+                    Unfortunately the form did not go through, try again or email me directly.
+                    carloscaceres041@gmail.com
+                    `)
+            }
+        }
+
+    }
     return (
         <section id="contact" className="contact-section-container">
             <div className="contact-section-content-container">
@@ -100,10 +192,30 @@ export default function Contact() {
                         {/* <h1>{textValues[index]}</h1> */}
                         <p className="contact-section-left-intersection-p-tag">Let's create something together!</p>
                         <div className="contact-section-left-socials">
-                            <button className="contact-section-social-buttons" onClick={(e)=>{contactButtonSubtmit(e, isMobile, "email")}}>Email</button>
-                            <button className="contact-section-social-buttons" onClick={(e)=>{contactButtonSubtmit(e, isMobile, "linkedin")}}>LinkedIn</button>
-                            <button className="contact-section-social-buttons" onClick={(e)=>{contactButtonSubtmit(e, isMobile, "github")}}>GitHub</button>
-                            <button className="contact-section-social-buttons" onClick={(e)=>{contactButtonSubtmit(e, isMobile, "youtube")}}>YouTube</button>
+                            <button 
+                                className="contact-section-social-buttons" 
+                                onClick={(e)=>{contactButtonSubtmit(e, isMobile, "email")}}
+                            >
+                                Email
+                            </button>
+                            <button 
+                                className="contact-section-social-buttons" 
+                                onClick={(e)=>{contactButtonSubtmit(e, isMobile, "linkedin")}}
+                            >
+                                LinkedIn
+                            </button>
+                            <button 
+                                className="contact-section-social-buttons" 
+                                onClick={(e)=>{contactButtonSubtmit(e, isMobile, "github")}}
+                            >
+                                GitHub
+                            </button>
+                            <button 
+                                className="contact-section-social-buttons" 
+                                onClick={(e)=>{contactButtonSubtmit(e, isMobile, "youtube")}}
+                            >
+                                YouTube
+                            </button>
                         </div>
                     </div>
                 </div>
@@ -111,28 +223,68 @@ export default function Contact() {
                     <h1>Send me a message</h1>
                     <Box sx={{ display: "flex", alignItems: "flex-end" }}>
                         <AccountCircle sx={{ color: "action.active", mr: 1, my: 0.5 }} />
-                        <TextField sx={{width: "280px"}} label="Full Name" variant="standard" required />
+                        <TextField 
+                            sx={{width: "280px"}} 
+                            label="Full Name" 
+                            variant="standard"
+                            value={fullName}
+                            onChange={(e)=>{setFullName(e.target.value)}}
+                            id="full-name"
+                            error={fullNameError}              
+                            helperText={fullNameError && "Please enter your full name"}
+                            className="input-fields"
+                            required />
                     </Box>
                     <Box sx={{ display: "flex", alignItems: "flex-end" }}>
                         <ContactMail sx={{ color: "action.active", mr: 1, my: 0.5 }} />
-                        <TextField sx={{width: "280px"}} label="Email" variant="standard" required />
+                        <TextField 
+                            sx={{width: "280px"}} 
+                            label="Email" 
+                            variant="standard"
+                            value={email}
+                            id="e-mail"
+                            error={emailError}
+                            helperText={emailError && "Please enter a valid email"}
+                            onChange={(e)=>{setEmail(e.target.value)}}
+                            className="input-fields"
+                            required />
                     </Box>
                     <Box sx={{ display: "flex", alignItems: "flex-end" }}>
                         <Subject sx={{ color: "action.active", mr: 1, my: 0.5 }} />
-                        <TextField sx={{width: "280px"}} label="Subject" variant="standard" required />
+                        <TextField 
+                            sx={{width: "280px"}} 
+                            label="Subject" 
+                            variant="standard"
+                            value={messageSubject} 
+                            onChange={(e)=>{setMessageSubject(e.target.value)}}
+                            error={subjectError}
+                            helperText={subjectError && "Please write a subject"}
+                            id="message-subject"
+                            className="input-fields"
+                            required />
                     </Box>
                     <Box sx={{ display: "flex", alignItems: "flex-end" }}>
                         <Engineering sx={{ color: "action.active", mr: 1, my: 0.5 }} />
                         <TextField
                             label={`Tell me more about ${textValuesForBox[indexTextBox]}`}
                             variant="standard"
+                            value={messageBody}
                             multiline
                             maxRows={7}
                             sx={{width: "280px"}}
-                            required
-                        />
+                            onChange={(e)=>{setMessageBody(e.target.value)}}
+                            id="message-body"
+                            error={bodyError}
+                            helperText={bodyError && "Please write a longer message"}
+                            className="input-fields"
+                            required />
                     </Box>
-                    <button className="contact-section-message-button" onClick={(e)=>{contactButtonSubtmit(e, isMobile, "message")}}>Send message</button>
+                    <button 
+                        className="contact-section-message-button" 
+                        onClick={(e)=>{sendMessage(e, fullName, email, messageSubject, messageBody)}}
+                    >
+                        Send message
+                    </button>
                 </div>
             </div>
         </section>
