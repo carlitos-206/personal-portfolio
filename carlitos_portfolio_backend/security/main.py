9234dd20a8bc43d2bf9b31ec662061fa0a4edb0f
carlitos-206 <carloscaceres041@gmail.com>
2025-03-17 16:46:44 -0700

    Linux migration

---
+++ carlitos_portfolio_backend/security/main.py
@@ -0,0 +1,9 @@
+#   This file contains security mesure to stop scrapers from accessing hidden files
+
+
+
+
+
+#   Set allowed routes
+#   [ '/', '/cover-letter', '/custom-gpt', '/voice-demo-init', '/trained-chat', '/robots.txt' ]
+#   
