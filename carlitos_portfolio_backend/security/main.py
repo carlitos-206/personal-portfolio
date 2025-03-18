ecf7e4173d14d554cd69c55f01f5ec90189dd940
carlitos-206 <carloscaceres041@gmail.com>
2025-03-18 16:48:33 -0700

    main security pseudo code

--- carlitos_portfolio_backend/security/main.py
+++ carlitos_portfolio_backend/security/main.py
@@ -1,9 +1,23 @@
-#   This file contains security mesure to stop scrapers from accessing hidden files
+#   This file contains security measure to stop scrapers from accessing hidden files
 
 
 
 
 
-#   Set allowed routes
-#   [ '/', '/cover-letter', '/custom-gpt', '/voice-demo-init', '/trained-chat', '/robots.txt' ]
-#   
+#   def SecurityFunc(Request, origin)
+#      Set allowed routes
+#           [ '/', '/cover-letter', '/custom-gpt', '/voice-demo-init', '/trained-chat', '/robots.txt' ]
+
+#      Parse request
+#           Ignore = Data
+#           Extract = API KEY
+#           Validate API KEY in Firebase
+#           Exclude rule for /robots.txt
+#           if ROUTE = TRUE and API KEY = TRUE 
+#               return 200
+#           if ROUTE = TRUE and API Key = FALSE
+#               return 500
+#           if ROUTE = '/robots.txt'
+#               return 200
+#           if ROUTE = FALSE and API KEY = FALSE
+#               return pain(Request, origin)
